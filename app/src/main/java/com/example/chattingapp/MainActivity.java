b27a9a73413c776047e474f19ec8e2b63bc9f20d
io-blip <66187@siswa.unimas.my>
2021-05-27 12:22:13 +0800

    Sign in/up/out #5

--- app/src/main/java/com/example/chattingapp/MainActivity.java
+++ app/src/main/java/com/example/chattingapp/MainActivity.java
@@ -1,77 +1,66 @@
 package com.example.chattingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.AlertDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.NotNull;
+
 
 public class MainActivity extends AppCompatActivity {
-    private EditText emailEt, passwordEt;
-    private AlertDialog.Builder builder;
-    private FirebaseAuth firebaseAuth;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        emailEt = findViewById(R.id.email);
-        passwordEt = findViewById(R.id.password);
-        Button signInButton = findViewById(R.id.login);
-        builder = new AlertDialog.Builder(this);
-        TextView signUpTv = findViewById(R.id.signUpTv);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        myRef = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
 
-        signInButton.setOnClickListener(v -> Login());
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                UsersModel users = snapshot.getValue(UsersModel.class);
+            }
 
-        signUpTv.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
-            startActivity(intent);
-            finish();
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
         });
     }
-        private void Login(){
 
-            String email = emailEt.getText().toString();
-            String password = passwordEt.getText().toString();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.home_menu,menu);
+        return true;
+    }
 
-            if(TextUtils.isEmpty(email)){
-                emailEt.setError("Enter your email");
-                return;
-            }
-            else if(TextUtils.isEmpty(password)){
-                passwordEt.setError("Enter your password");
-                return;
-            }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-            firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, task -> {
+        switch (item.getItemId()){
 
-                if(task.isSuccessful()){
-                    builder.setMessage("Please wait...");
-                    builder.show();
-                    Toast.makeText(MainActivity.this,"Login successfully!",Toast.LENGTH_LONG).show();
-                    Intent intent=new Intent(MainActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-
-                else{
-                    Toast.makeText(MainActivity.this,"Failed to sign in, password may be incorrect!",Toast.LENGTH_LONG).show();
-                }
-            });
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                finish();
+                return true;
         }
+        return false;
+    }
 }
---
+++ app/src/main/java/com/example/chattingapp/RegisterActivity.java
@@ -0,0 +1,111 @@
+package com.example.chattingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button Register;
+    private EditText UserEmail,UserPassword, Username;
+    private TextView SignIn;
+    private FirebaseAuth auth;
+    private DatabaseReference myRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        auth = FirebaseAuth.getInstance();
+        myRef = FirebaseDatabase.getInstance().getReference();
+
+        Register = (Button) findViewById(R.id.register);
+        UserEmail = (EditText) findViewById(R.id.email);
+        Username = (EditText) findViewById(R.id.username);
+        UserPassword = (EditText) findViewById(R.id.password1);
+        SignIn = (TextView) findViewById(R.id.signIn);
+
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+                finish();
+            }
+        });
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = Username.getText().toString();
+                String email = UserEmail.getText().toString();
+                String password = UserPassword.getText().toString();
+
+                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(RegisterActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    Register(username,email,password);
+                }
+            }
+        });
+    }
+        private void Register(final String Username, String UserEmail, String UserPassword){
+            auth.createUserWithEmailAndPassword(UserEmail, UserPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userid = firebaseUser.getUid();
+
+                            myRef = FirebaseDatabase.getInstance()
+                                    .getReference("MyUsers").child(userid);
+
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("id", userid);
+                            hashMap.put("username",Username);
+                            hashMap.put("imageURL","default");
+
+                            myRef.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                  @Override
+                                  public void onComplete(@NonNull Task<Void> task) {
+
+                                      if (task.isSuccessful()){
+                                          Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                                          mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                                          startActivity(mainIntent);
+                                          finish();
+                                          Toast.makeText(RegisterActivity.this, "Account Created Successfully...", Toast.LENGTH_SHORT).show();
+                                      }
+                                  }
+                              });
+                        }else{
+                            String message = Objects.requireNonNull(task.getException()).toString();
+                            Toast.makeText(RegisterActivity.this, "Error : " + message, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        }
+}
+
+
